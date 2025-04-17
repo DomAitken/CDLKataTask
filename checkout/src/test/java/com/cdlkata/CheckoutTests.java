69b6987b5ff26e93dcbeeac2a818368195ca5ab6
Dom Aitken <daitken123@hotmail.com>
2025-04-17 23:27:05 +0100

    Commit 002: Created test branch, updated pom and created test scenarios

---
+++ checkout/src/test/java/com/cdlkata/CheckoutTests.java
@@ -0,0 +1,34 @@
+package com.cdlkata;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+
+public class CheckoutTests {
+
+    @Test
+    public void testSingleItems() {
+
+    }
+
+    @Test
+    public void testSpecialPriceForA() {
+
+    }
+
+    @Test
+    public void testSpecialPriceForB() {
+
+    }
+
+    @Test
+    public void testMixOfItems() {
+
+    }
+
+    @Test
+    public void testInvalidSkuThrowsException() {
+
+    }
+}
